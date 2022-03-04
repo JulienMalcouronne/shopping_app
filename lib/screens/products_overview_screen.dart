84e2f7bdc21c937113162edb12ec9388ad83c2e8
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-03-04 11:13:21 +0100

    increment dynamically the cart

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
 import '../widget/products_grid.dart';
 // import '../providers/products_provider.dart';
+import '../widget/badge.dart';
+import '../providers/cart.dart';
 
 enum FilterOptions {
   Favorites,
@@ -51,7 +53,19 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
             icon: Icon(
               Icons.more_vert,
             ),
-          )
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
       body: ProductsGrid(_showOnlyFavorites),
