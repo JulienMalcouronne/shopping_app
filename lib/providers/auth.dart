201f98e915638c1e3370c516f5fa91620d199d3e
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-03-06 21:46:04 +0100

    added error handler on login and signup + some refacto

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,26 +4,42 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
-  static const params = {'key': 'AIzaSyCn7HDhcqH410kJZxkfv3Q2HTESeJ4v0v4n'};
+  static const params = {'key': 'AIzaSyCn7HDhcqH410kJZxkfv3Q2HTESeJ4v0v4'};
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    var url = Uri.https('identitytoolkit.googleapis.com', urlSegment, params);
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
 
   Future<void> signup(String email, String password) async {
-    final url = Uri.https(
-        'identitytoolkit.googleapis.com', '/v1/accounts:signUp', params);
+    return _authenticate(email, password, '/v1/accounts:signUp');
+  }
 
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, '/v1/accounts:signInWithPassword');
   }
 }
